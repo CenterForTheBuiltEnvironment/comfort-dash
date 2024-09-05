4e01f4e0f4cdf25ad64d5d4640d16628bb155a88
Jinjia Bai <jbai0252@uni.sydney.edu.au>
2024-09-05 22:41:35 +1000

    Revert "fix code style"
    This reverts commit 87477e12e3c4079a163740cb2074c7379ca5284f.

--- components/show_results.py
+++ components/show_results.py
@@ -10,6 +10,20 @@ from utils.my_config_file import (
     ElementsIDs,
 )
 
+def find_dict_with_key_value(d, key, value):
+        if isinstance(d, dict):
+            if d.get(key) == value:
+                return d
+            for k, v in d.items():
+                result = find_dict_with_key_value(v, key, value)
+                if result is not None:
+                    return result
+        elif isinstance(d, list):
+            for item in d:
+                result = find_dict_with_key_value(item, key, value)
+                if result is not None:
+                    return result
+        return None
 
 def display_results(selected_model: str, form_content: dict, units_selection: str):
 
