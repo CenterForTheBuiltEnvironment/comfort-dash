2ee3abbbc32efb8391cd12277c6b10f2992454ee
Jinjia Bai <jbai0252@uni.sydney.edu.au>
2024-09-05 13:29:16 +1000

    key_altermative_position

--- components/show_results.py
+++ components/show_results.py
@@ -9,12 +9,10 @@ from utils.my_config_file import (
     UnitSystem,
     convert_units,
     UnitConverter,
+    ElementsIDs,
 )
 
-
-def display_results(selected_model: str, form_content: dict, units_selection: str):
-    # todo the following function should be moved outside the code
-    def find_dict_with_key_value(d, key, value):
+def find_dict_with_key_value(d, key, value):
         if isinstance(d, dict):
             if d.get(key) == value:
                 return d
@@ -29,36 +27,38 @@ def display_results(selected_model: str, form_content: dict, units_selection: st
                     return result
         return None
 
+def display_results(selected_model: str, form_content: dict, units_selection: str):
     if selected_model is None:
         return no_update
 
     # creating a copy of the model inputs
-    list_model_inputs = deepcopy(Models[selected_model].value.inputs)
+    model_inputs_dict = {input.id: input for input in Models[selected_model].value.inputs}
 
     # updating the values of the model inputs with the values from the form
-    for model_input in list_model_inputs:
-        model_input.value = find_dict_with_key_value(
+    for model_input in model_inputs_dict.values():
+        found_input = find_dict_with_key_value(
             form_content, "id", model_input.id
-        )["value"]
+        )
+        if found_input is not None:
+            model_input.value = found_input["value"]
 
     # converting the units if necessary
     units = UnitSystem.IP.value if units_selection else UnitSystem.SI.value
     if units == UnitSystem.IP.value:
-        list_model_inputs = convert_units(list_model_inputs, UnitSystem.SI.value)
+        model_inputs_dict = convert_units(list(model_inputs_dict.values()), UnitSystem.SI.value)
 
     results = []
     columns: int = 2
     if selected_model == Models.PMV_EN.name:
         columns = 2
 
-        # todo I do not like we are unpacking the values from the list using position, we should use the key
         r_pmv = pmv_ppd(
-            tdb=list_model_inputs[0].value,
-            tr=list_model_inputs[1].value,
-            vr=list_model_inputs[2].value,
-            rh=list_model_inputs[3].value,
-            met=list_model_inputs[4].value,
-            clo=list_model_inputs[5].value,
+            tdb=model_inputs_dict[ElementsIDs.t_db_input.value].value,
+            tr=model_inputs_dict[ElementsIDs.t_r_input.value].value,
+            vr=model_inputs_dict[ElementsIDs.v_input.value].value,
+            rh=model_inputs_dict[ElementsIDs.rh_input.value].value,
+            met=model_inputs_dict[ElementsIDs.met_input.value].value,
+            clo=model_inputs_dict[ElementsIDs.clo_input.value].value,
             wme=0,
             limit_inputs=False,
             standard="ISO",
@@ -68,14 +68,13 @@ def display_results(selected_model: str, form_content: dict, units_selection: st
     elif selected_model == Models.PMV_ashrae.name:
         columns = 2
 
-        # todo I do not like we are unpacking the values from the list using position, we should use the key
         r_pmv = pmv_ppd(
-            tdb=list_model_inputs[0].value,
-            tr=list_model_inputs[1].value,
-            vr=list_model_inputs[2].value,
-            rh=list_model_inputs[3].value,
-            met=list_model_inputs[4].value,
-            clo=list_model_inputs[5].value,
+            tdb=model_inputs_dict[ElementsIDs.t_db_input.value].value,
+            tr=model_inputs_dict[ElementsIDs.t_r_input.value].value,
+            vr=model_inputs_dict[ElementsIDs.v_input.value].value,
+            rh=model_inputs_dict[ElementsIDs.rh_input.value].value,
+            met=model_inputs_dict[ElementsIDs.met_input.value].value,
+            clo=model_inputs_dict[ElementsIDs.clo_input.value].value,
             wme=0,
             limit_inputs=False,
             standard="ashrae",
@@ -85,29 +84,28 @@ def display_results(selected_model: str, form_content: dict, units_selection: st
     elif selected_model == Models.Adaptive_ASHRAE.name:
         columns = 1
 
-        # todo I do not like we are unpacking the values from the list using position, we should use the key
         adaptive = adaptive_ashrae(
-            tdb=list_model_inputs[0].value,
-            tr=list_model_inputs[1].value,
-            t_running_mean=list_model_inputs[2].value,
-            v=list_model_inputs[3].value,
+            tdb=model_inputs_dict[ElementsIDs.t_db_input.value].value,
+            tr=model_inputs_dict[ElementsIDs.t_r_input.value].value,
+            t_running_mean=model_inputs_dict[ElementsIDs.t_rm_input.value].value,
+            v=model_inputs_dict[ElementsIDs.v_input.value].value,
         )
         if units == UnitSystem.IP.value:
             adaptive.tmp_cmf = round(
                 UnitConverter.celsius_to_fahrenheit(adaptive.tmp_cmf), 2
-            )
+                )
             adaptive.tmp_cmf_80_low = round(
                 UnitConverter.celsius_to_fahrenheit(adaptive.tmp_cmf_80_low), 2
-            )
+                )
             adaptive.tmp_cmf_80_up = round(
                 UnitConverter.celsius_to_fahrenheit(adaptive.tmp_cmf_80_up), 2
-            )
+                )
             adaptive.tmp_cmf_90_low = round(
                 UnitConverter.celsius_to_fahrenheit(adaptive.tmp_cmf_90_low), 2
-            )
+                )
             adaptive.tmp_cmf_90_up = round(
                 UnitConverter.celsius_to_fahrenheit(adaptive.tmp_cmf_90_up), 2
-            )
+                )
         results.append(dmc.Center(dmc.Text(f"Comfort temperature: {adaptive.tmp_cmf}")))
         results.append(
             dmc.Center(
@@ -126,15 +124,14 @@ def display_results(selected_model: str, form_content: dict, units_selection: st
     elif selected_model == Models.PHS.name:
         columns = 1
 
-        # todo I do not like we are unpacking the values from the list using position, we should use the key
         phs_result = phs(
-            tdb=list_model_inputs[0].value,
-            tr=list_model_inputs[1].value,
-            v=list_model_inputs[2].value,
-            rh=list_model_inputs[3].value,
-            met=list_model_inputs[4].value,
-            clo=list_model_inputs[5].value,
-            posture=list_model_inputs[6].value,
+            tdb=model_inputs_dict[ElementsIDs.t_db_input.value].value,
+            tr=model_inputs_dict[ElementsIDs.t_r_input.value].value,
+            v=model_inputs_dict[ElementsIDs.v_input.value].value,
+            rh=model_inputs_dict[ElementsIDs.rh_input.value].value,
+            met=model_inputs_dict[ElementsIDs.met_input.value].value,
+            clo=model_inputs_dict[ElementsIDs.clo_input.value].value,
+            posture=model_inputs_dict[ElementsIDs.posture_input.value].value,
             wme=0,
             limit_inputs=False,
         )
