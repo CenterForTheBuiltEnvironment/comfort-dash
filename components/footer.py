86460ec724bcf32684d54591ce3bfdd94a5eb38c
Jinjia Bai <jbai0252@uni.sydney.edu.au>
2024-09-06 12:43:01 +1000

    fix error in Manual rollback

--- components/footer.py
+++ components/footer.py
@@ -1,32 +1,106 @@
-import dash_mantine_components as dmc
+from dash import html
 
-from utils.my_config_file import Functionalities
-from utils.website_text import TextHome
+from utils.website_text import TextFooter
 
 
-def functionality_selection():
-    return dmc.Center(
-        [
-            dmc.Text(TextHome.functionality_selection.value, mr="sm"),
-            dmc.SegmentedControl(
-                value=Functionalities.Default.value,
-                data=[
-                    {
-                        "value": Functionalities.Default.value,
-                        "label": Functionalities.Default.value,
-                    },
-                    {
-                        "value": Functionalities.Compare.value,
-                        "label": Functionalities.Compare.value,
-                    },
-                    {
-                        "value": Functionalities.Ranges.value,
-                        "label": Functionalities.Ranges.value,
-                    },
+def my_footer():
+    return html.Div(
+        children=[
+            html.Footer(
+                className="cbe-footer-content",
+                children=[
+                    # todo do not use html divs but use dash mantine components
+                    html.Div(
+                        className="cbe-footer-logo",
+                        children=[
+                            html.Img(
+                                src="assets/media/CBE-logo-2019-white.png",
+                                alt="CBE Logo",
+                            ),
+                            html.Img(
+                                src="assets/media/ucb-logo-2024-white.png",
+                                alt="UC Berkeley Logo",
+                            ),
+                        ],
+                    ),
+                    # todo do not use html Nav in the footer, use dash mantine components
+                    html.Nav(
+                        # todo we should not change the appearance of the logo using css, we should use dash mantine components
+                        className="cbe-footer-links",
+                        children=[
+                            html.A(
+                                "Contact Us",
+                                href=TextFooter.contact_us_link.value,
+                                target="_blank",
+                            ),
+                            html.A(
+                                "Report Issues",
+                                href=TextFooter.report_issues_link.value,
+                                target="_blank",
+                            ),
+                        ],
+                    ),
+                    html.Nav(
+                        className="cbe-social-links",
+                        children=[
+                            html.A(
+                                html.Img(
+                                    src="assets/media/github-white-transparent.png",
+                                    alt="GitHub",
+                                ),
+                                href="#",
+                            ),
+                            html.A(
+                                html.Img(
+                                    src="assets/media/linkedin-white.png",
+                                    alt="LinkedIn",
+                                ),
+                                href="#",
+                            ),
+                        ],
+                        # todo do not use style, use dash mantine components
+                        style={"position": "relative", "left": "-9px"},
+                    ),
+                    html.Div(
+                        className="cbe-citation-info",
+                        children=[
+                            html.Strong(TextFooter.cite_strong.value),
+                            html.Div(
+                                [
+                                    TextFooter.cite.value,
+                                    html.A(
+                                        TextFooter.cite_link.value,
+                                        href=TextFooter.cite_link.value,
+                                        className="cbe-doi-link",
+                                        target="_blank",
+                                    ),
+                                ]
+                            ),
+                        ],
+                    ),
                 ],
-                mb=10,
-                radius="lg",
-                transitionDuration=500,
+            ),
+            html.Div(
+                className="cbe-footnotes",
+                children=[
+                    html.Div(
+                        TextFooter.copy_right.value,
+                    ),
+                    html.Div(
+                        className="cbe-version-license",
+                        children=[
+                            html.Div(TextFooter.version.value),
+                            html.A(
+                                html.Img(
+                                    src="https://img.shields.io/badge/License-MIT-yellow.svg",
+                                    alt="License: MIT",
+                                    style={"width": "65px", "height": "18px"},
+                                ),
+                                href=TextFooter.open_source_link.value,
+                            ),
+                        ],
+                    ),
+                ],
             ),
         ]
     )
