9ccd6a2d068bbb3cb5b21edb50e6bf958055c80c
Jinjia Bai <jbai0252@uni.sydney.edu.au>
2024-09-08 23:16:58 +1000

    Fix: SI/IP error

--- utils/get_inputs.py
+++ utils/get_inputs.py
@@ -4,43 +4,43 @@ from dash import no_update
 
 from utils.my_config_file import Models, UnitSystem, convert_units, ElementsIDs
 
+'''
+# todo the following function should be moved outside the code
+def find_dict_with_key_value(d, key, value):
+    if isinstance(d, dict):
+        if d.get(key) == value:
+            return d
+        for k, v in d.items():
+            result = find_dict_with_key_value(v, key, value)
+            if result is not None:
+                return result
+    elif isinstance(d, list):
+        for item in d:
+            result = find_dict_with_key_value(item, key, value)
+            if result is not None:
+                return result
+    return None
+'''
 
 def get_inputs(selected_model: str, form_content: dict, units: str):
-    '''
-    # todo the following function should be moved outside the code
-    def find_dict_with_key_value(d, key, value):
-        if isinstance(d, dict):
-            if d.get(key) == value:
-                return d
-            for k, v in d.items():
-                result = find_dict_with_key_value(v, key, value)
-                if result is not None:
-                    return result
-        elif isinstance(d, list):
-            for item in d:
-                result = find_dict_with_key_value(item, key, value)
-                if result is not None:
-                    return result
-        return None
-    '''
+
     if selected_model is None:
         return no_update
 
     # creating a copy of the model inputs
     list_model_inputs = deepcopy(Models[selected_model].value.inputs)
-
+    
     # Update input values ​​to map dynamic IDs to ElementsIDs
-    inputs = {}
     for model_input in list_model_inputs:
         found_input = form_content.get(model_input.id)
-        if found_input:
-            # 使用 ElementsIDs 的值作为键
-            for element in ElementsIDs:
-                if model_input.id == element.value:
-                    inputs[element.value] = found_input["value"]
+        model_input.value = found_input["value"]
     
     # converting the units if necessary
     if units == UnitSystem.IP.value:
         list_model_inputs = convert_units(list_model_inputs, UnitSystem.SI.value)
 
+    inputs = {}
+    for model_input in list_model_inputs:
+        inputs[model_input.id] = model_input.value
+    
     return inputs
