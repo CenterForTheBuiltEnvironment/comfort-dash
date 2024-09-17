567ea30770c56764c75963971e8d7a3601421033
Ruixin Wu <524463257@qq.com>
2024-09-18 06:52:33 +1000

    ut

--- utils/get_inputs.py
+++ utils/get_inputs.py
@@ -26,92 +26,6 @@ def find_dict_with_key_value(d, key, value):
     return None
 
 
-# def extract_float(value):
-#     if isinstance(value, (int, float)):
-#         return float(value)
-#     if isinstance(value, str):
-#         try:
-#             return float(value.split(":")[-1].strip().split()[0])
-#         except ValueError:
-#             return None
-#     return None
-
-
-# def get_inputs(selected_model: str, form_content: dict, units: str, function_selection: str):
-#     if selected_model is None:
-#         return no_update
-
-#     # creating a copy of the model inputs
-#     list_model_inputs = deepcopy(Models[selected_model].value.inputs)
-
-#     # converting the units if necessary
-#     if units == UnitSystem.IP.value:
-#         list_model_inputs = convert_units(list_model_inputs, UnitSystem.SI.value)
-
-#     # updating the values of the model inputs with the values from the form
-#     inputs = {}
-#     for model_input in list_model_inputs:
-#         default_value = model_input.value
-#         input_dict = find_dict_with_key_value(form_content, "id", model_input.id)
-
-#         if input_dict and "value" in input_dict:
-#             original_value = input_dict["value"]
-#             converted_value = extract_float(str(original_value))
-
-#             if (
-#                 converted_value is not None
-#                 and model_input.min <= converted_value <= model_input.max
-#             ):
-#                 inputs[model_input.id] = converted_value
-#             else:
-#                 inputs[model_input.id] = default_value
-#         else:
-#             inputs[model_input.id] = default_value
-#     return inputs
-
-
-# def get_inputs(selected_model: str, form_content: dict, units: str, functionality_selection: str):
-#     if selected_model is None:
-#         return no_update
-
-#     # Creating a copy of the model inputs
-#     list_model_inputs = deepcopy(Models[selected_model].value.inputs)
-
-#     # Check if in Compare mode and concatenate inputs2
-#     if functionality_selection == Functionalities.Compare.value:
-#         list_model_inputs2 = deepcopy(Models[selected_model].value.inputs2)
-#         # Combine inputs and inputs2 into a single list
-#         combined_model_inputs = list_model_inputs + list_model_inputs2
-#     else:
-#         combined_model_inputs = list_model_inputs
-
-#     # Converting the units if necessary
-#     if units == UnitSystem.IP.value:
-#         combined_model_inputs = convert_units(combined_model_inputs, UnitSystem.SI.value)
-
-#     # Processing the combined list of inputs
-#     inputs = {}
-#     for model_input in combined_model_inputs:
-#         default_value = model_input.value
-#         input_dict = find_dict_with_key_value(form_content, "id", model_input.id)
-
-#         if input_dict and "value" in input_dict:
-#             original_value = input_dict["value"]
-#             converted_value = extract_float(str(original_value))
-
-#             if (
-#                 converted_value is not None
-#                 and model_input.min <= converted_value <= model_input.max
-#             ):
-#                 inputs[model_input.id] = converted_value
-#             else:
-#                 inputs[model_input.id] = default_value
-#         else:
-#             inputs[model_input.id] = default_value
-
-#     return inputs
-
-
 def extract_float(value):
     if isinstance(value, (int, float)):
         return float(value)
@@ -134,26 +48,21 @@ def get_inputs(
     if selected_model is None:
         return no_update
 
-    # Creating a copy of the model inputs
     list_model_inputs = deepcopy(Models[selected_model].value.inputs)
 
-    # Check if in Compare mode and concatenate inputs2
     if functionality_selection == Functionalities.Compare.value and selected_model in [
         Models.PMV_ashrae.name
     ]:
         list_model_inputs2 = deepcopy(Models[selected_model].value.inputs2)
-        # Combine inputs and inputs2 into a single list
         combined_model_inputs = list_model_inputs + list_model_inputs2
     else:
         combined_model_inputs = list_model_inputs
 
-    # Converting the units if necessary
     if units == UnitSystem.IP.value:
         combined_model_inputs = convert_units(
             combined_model_inputs, UnitSystem.SI.value
         )
 
-    # Processing the combined list of inputs
     inputs = {}
     for model_input in combined_model_inputs:
         default_value = model_input.value
@@ -174,5 +83,4 @@ def get_inputs(
         else:
             inputs[input_id] = default_value
 
-    # print(f"Final inputs collected: {inputs}")
     return inputs
