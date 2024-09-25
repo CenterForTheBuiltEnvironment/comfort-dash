fafde27e6a192b245d63e6358caac5cc5728b3db
Marcusxixi <1654400916@qq.com>
2024-09-25 18:12:09 +1000

    fix bug (make the air speed drop down box style with generate_dropdown_inline) modify the generate_dropdown_inline function by adding a parameter on show dropdown only, set the default v_input value to 0.1 x axis can move now

--- components/drop_down_inline.py
+++ components/drop_down_inline.py
@@ -2,7 +2,24 @@ import dash_bootstrap_components as dbc
 from dash import dcc, html
 
 
-def generate_dropdown_inline(questions_to_display, value=None, clearable=True):
+def generate_dropdown_inline(
+    questions_to_display, value=None, clearable=True, only_dropdown=False
+):
+    dropdown = dbc.Col(
+        dcc.Dropdown(
+            options=questions_to_display["options"],
+            value=(value if value is not None else questions_to_display["default"]),
+            multi=questions_to_display["multi"],
+            id=questions_to_display["id"],
+            optionHeight=30,
+            # todo when possible, avoid using inline styles. Define a global style for the dropdowns
+            style={"lineHeight": "1.1rem"},
+            clearable=clearable,
+        ),
+    )
+    if only_dropdown:
+        return dropdown
+
     return dbc.Row(
         [
             dbc.Col(
@@ -12,20 +29,7 @@ def generate_dropdown_inline(questions_to_display, value=None, clearable=True):
                 ),
                 width="auto",
             ),
-            dbc.Col(
-                dcc.Dropdown(
-                    options=questions_to_display["options"],
-                    value=(
-                        value if value is not None else questions_to_display["default"]
-                    ),
-                    multi=questions_to_display["multi"],
-                    id=questions_to_display["id"],
-                    optionHeight=30,
-                    # todo when possible, avoid using inline styles. Define a global style for the dropdowns
-                    style={"lineHeight": "1.1rem"},
-                    clearable=clearable,
-                ),
-            ),
+            dropdown,
         ],
         className="pb-2",
     )
--- components/input_environmental_personal.py
+++ components/input_environmental_personal.py
@@ -1,6 +1,7 @@
 import dash
 import dash_mantine_components as dmc
 from dash import html, callback, Output, Input, State
+from components.drop_down_inline import generate_dropdown_inline
 
 from components.dropdowns import options
 from utils.my_config_file import (
@@ -348,10 +349,12 @@ def input_environmental_personal(
         if input_id in all_inputs:
             default_input = None
             input_stack = None
-
             if input_id in {ElementsIDs.met_input.value, ElementsIDs.clo_input.value}:
                 default_input = create_autocomplete(values)
-            elif input_id == ElementsIDs.v_input.value:
+            elif (
+                selected_model == Models.Adaptive_EN.name
+                and input_id == ElementsIDs.v_input.value
+            ):
                 default_input = create_select_component(values)
             else:
                 default_input = dmc.NumberInput(
@@ -535,12 +538,14 @@ def create_autocomplete(values: ModelInputsInfo):
 
 
 def create_select_component(values: ModelInputsInfo):
-    return dmc.Select(
-        id=values.id,
-        placeholder=f"Please select a {values.name}",
-        data=[],
-        value=str(values.value),
-    )
+    air_speed_box = {
+        "id": ElementsIDs.v_input.value,
+        "question": None,
+        "options": [speed.value for speed in AdaptiveENSpeeds],
+        "multi": False,
+        "default": values.value,
+    }
+    return generate_dropdown_inline(air_speed_box, clearable=False, only_dropdown=True)
 
 
 # Todo determine if the value is over the maximum
