b6c789e03c444b815cbacbc83c4f34ee140e3bf6
Jinjia Bai <jbai0252@uni.sydney.edu.au>
2024-10-09 02:09:16 +1100

    Reapply "SET update"
    This reverts commit 371443cada872f3f2c56daf1d1313a2dbe5d596c.

--- pages/home.py
+++ pages/home.py
@@ -2,7 +2,20 @@ import dash
 import dash_mantine_components as dmc
 from dash import html, callback, Output, Input, no_update, State, ctx, dcc
 
-from components.charts import t_rh_pmv, chart_selector, adaptive_en_chart
+
+from components.charts import (
+    t_rh_pmv,
+    chart_selector,
+    adaptive_chart,
+    generate_tdb_hr_chart,
+    SET_outputs_chart,
+    speed_temp_pmv,
+    get_heat_losses,
+    psy_ashrae_pmv,
+    generate_operative_chart,
+    psy_ashrae_pmv_operative,
+)
+
 from components.dropdowns import (
     model_selection,
 )
@@ -23,7 +36,7 @@ from utils.my_config_file import (
     Functionalities,
 )
 import plotly.graph_objects as go
-from urllib.parse import parse_qs, urlencode
+from pythermalcomfort.psychrometrics import psy_ta_rh
 
 dash.register_page(__name__, path=URLS.HOME.value)
 
@@ -65,8 +78,6 @@ layout = dmc.Stack(
                                 id=ElementsIDs.CHART_CONTAINER.value,
                             ),
                             dmc.Text(id=ElementsIDs.note_model.value),
-                            dcc.Location(id=ElementsIDs.URL.value, refresh=False),
-                            dcc.Store(id=ElementsIDs.INITIAL_URL.value, storage_type="memory"),
                         ],
                     ),
                     span={"base": 12, "sm": Dimensions.right_container_width.value},
@@ -79,10 +90,8 @@ layout = dmc.Stack(
 
 
 # Todo adding reflecting value to the url
-# done
 @callback(
     Output(MyStores.input_data.value, "data"),
-    Output(ElementsIDs.URL.value, "search", allow_duplicate=True),
     Input(ElementsIDs.inputs_form.value, "n_clicks"),
     Input(ElementsIDs.inputs_form.value, "children"),
     Input(ElementsIDs.clo_input.value, "value"),
@@ -91,7 +100,6 @@ layout = dmc.Stack(
     Input(ElementsIDs.chart_selected.value, "value"),
     Input(ElementsIDs.functionality_selection.value, "value"),
     State(ElementsIDs.MODEL_SELECTION.value, "value"),
-    prevent_initial_call=True,
 )
 def update_store_inputs(
     form_clicks: int,
@@ -104,21 +112,24 @@ def update_store_inputs(
     selected_model: str,
 ):
     units = UnitSystem.IP.value if units_selection else UnitSystem.SI.value
-    inputs = get_inputs(
-        selected_model, form_content, units, functionality_selection, type="input"
-    )
+    inputs = get_inputs(selected_model, form_content, units, functionality_selection)
+
+    if ctx.triggered:
+        triggered_id = ctx.triggered[0]["prop_id"].split(".")[0]
+        if triggered_id == ElementsIDs.clo_input.value and clo_value != "":
+            inputs[ElementsIDs.clo_input.value] = float(clo_value)
+        if triggered_id == ElementsIDs.met_input.value and met_value != "":
+            inputs[ElementsIDs.met_input.value] = float(met_value)
 
     inputs[ElementsIDs.UNIT_TOGGLE.value] = units
     inputs[ElementsIDs.MODEL_SELECTION.value] = selected_model
     inputs[ElementsIDs.chart_selected.value] = chart_selected
     inputs[ElementsIDs.functionality_selection.value] = functionality_selection
 
-    url_search = f"?{urlencode(inputs)}"
-
-    return inputs, url_search
+    return inputs
 
 
-# keep data persistent in the store
+# todo get the value from the url
 @callback(
     Output(ElementsIDs.INPUT_SECTION.value, "children"),
     Input(ElementsIDs.MODEL_SELECTION.value, "value"),
@@ -126,54 +137,11 @@ def update_store_inputs(
     Input(ElementsIDs.functionality_selection.value, "value"),
 )
 def update_inputs(selected_model, units_selection, function_selection):
+    # todo here I should first check if some inputs are already stored in the store
     if selected_model is None:
         return no_update
     units = UnitSystem.IP.value if units_selection else UnitSystem.SI.value
-    return (
-        input_environmental_personal(selected_model, units, function_selection),
-        selected_model,
-        units_selection,
-        function_selection,
-    )
-
-
-# once function: update_inputs via URL, update the value of the model dropdown, unit toggle and functionality dropdown and chart dropdown, and inputs, it only use once when the page is loaded
-@callback(
-    Output(ElementsIDs.MODEL_SELECTION.value, "value"),
-    Output(ElementsIDs.INPUT_SECTION.value, "children", allow_duplicate=True),
-    Output(ElementsIDs.chart_selected.value, "value"),
-    Output(ElementsIDs.functionality_selection.value, "value"),
-    Output(ElementsIDs.UNIT_TOGGLE.value, "checked"),
-    Output(ElementsIDs.INITIAL_URL.value, "data"),
-    Input(ElementsIDs.URL.value, "search"),
-    State(ElementsIDs.INITIAL_URL.value, "data"),
-    prevent_initial_call=True,
-)
-def update_page_from_url(url_search, url_initialized):
-    if url_initialized or url_search is None:
-        return no_update, no_update, no_update, no_update, no_update, no_update
-
-    url_params = parse_qs(url_search.lstrip("?"))
-    url_params = {k: v[0] if len(v) == 1 else v for k, v in url_params.items()}
-
-    selected_model = url_params.get(ElementsIDs.MODEL_SELECTION.value)
-    if selected_model is None:
-        return no_update, no_update, no_update, no_update, no_update, no_update
-    units = url_params.get(ElementsIDs.UNIT_TOGGLE.value)
-    function_selection = url_params.get(ElementsIDs.functionality_selection.value)
-    chart_selected = url_params.get(ElementsIDs.chart_selected.value)
-    inputs = get_inputs(
-        selected_model, url_params, units, function_selection, type="url"
-    )
-
-    return (
-        selected_model,
-        input_environmental_personal(selected_model, units, function_selection),
-        chart_selected,
-        function_selection,
-        units == UnitSystem.IP.value,
-        True,  # Mark URL as initialized
-    )
+    return input_environmental_personal(selected_model, units, function_selection)
 
 
 @callback(
@@ -196,16 +164,93 @@ def update_note_model(selected_model):
     Output(ElementsIDs.charts_dropdown.value, "children"),
     Input(ElementsIDs.MODEL_SELECTION.value, "value"),
     Input(ElementsIDs.functionality_selection.value, "value"),
-    Input(ElementsIDs.chart_selected.value, "value"),
 )
-def update_note_model(selected_model, function_selection, chart_selected):
+def update_note_model(selected_model, function_selection):
     if selected_model is None:
         return no_update
     return chart_selector(
-        selected_model=selected_model,
-        function_selection=function_selection,
-        chart_selected=chart_selected,
+        selected_model=selected_model, function_selection=function_selection
     )
+
+
+#  double check the calculating method from pythermalcomfort lib, especially the units
+last_valid_annotation = None
+
+
+# @callback(
+#     Output(ElementsIDs.CHART_CONTAINER.value, "children"),
+#     Input(MyStores.input_data.value, "data"),
+#     Input(ElementsIDs.GRAPH_HOVER.value, "hoverData"),
+#     State(ElementsIDs.GRAPH_HOVER.value, "figure"),
+#     Input(ElementsIDs.functionality_selection.value, "value"),
+#     State(MyStores.input_data.value, "data"),
+# )
+@callback(
+    Output(ElementsIDs.GRAPH_HOVER.value, "figure"),
+    Input(ElementsIDs.GRAPH_HOVER.value, "hoverData"),
+    State(ElementsIDs.GRAPH_HOVER.value, "figure"),
+    State(MyStores.input_data.value, "data"),
+)
+def update_hover_annotation(hover_data, figure, inputs):
+    # For ensure tdp never shown as nan value
+    global last_valid_annotation
+
+    if (
+        hover_data
+        and figure
+        and "points" in hover_data
+        and len(hover_data["points"]) > 0
+    ):
+        chart_selected = inputs[ElementsIDs.chart_selected.value]
+
+        # not show annotation for adaptive methods
+        if chart_selected in [
+            Charts.psychrometric.value.name,
+            Charts.t_rh.value.name,
+            Charts.psychrometric_operative.value.name,
+        ]:
+            point = hover_data["points"][0]
+
+            if "x" in point and "y" in point:
+                t_db = point["x"]
+                rh = point["y"]
+
+                # check if y <= 0
+                if rh <= 0:
+                    if (
+                        last_valid_annotation is not None
+                        and "annotations" in figure["layout"]
+                    ):
+                        figure["layout"]["annotations"][0][
+                            "text"
+                        ] = last_valid_annotation
+                    return figure
+
+                # calculations
+                psy_results = psy_ta_rh(t_db, rh)
+                t_wb_value = psy_results.t_wb
+                t_dp_value = psy_results.t_dp
+                wa = psy_results.hr * 1000  # convert to g/kgda
+                h = psy_results.h / 1000  # convert to kj/kg
+
+                annotation_text = (
+                    f"t<sub>db</sub>: {t_db:.1f} °C<br>"
+                    f"RH: {rh:.1f} %<br>"
+                    f"W<sub>a</sub>: {wa:.1f} g<sub>w</sub>/kg<sub>da</sub><br>"
+                    f"t<sub>wb</sub>: {t_wb_value:.1f} °C<br>"
+                    f"t<sub>dp</sub>: {t_dp_value:.1f} °C<br>"
+                    f"h: {h:.1f} kJ/kg<br>"
+                )
+
+                if (
+                    "annotations" in figure["layout"]
+                    and len(figure["layout"]["annotations"]) > 0
+                ):
+                    figure["layout"]["annotations"][0]["text"] = annotation_text
+            else:
+                print("Unexpected hover data structure:", point)
+
+    return figure
 
 
 @callback(
@@ -242,7 +287,7 @@ def update_chart(inputs: dict, function_selection: str):
             )
         elif (
             selected_model == Models.PMV_ashrae.name
-            and function_selection != Functionalities.Ranges.value
+            and function_selection == Functionalities.Default.value
         ):
             image = t_rh_pmv(
                 inputs=inputs,
@@ -250,12 +295,64 @@ def update_chart(inputs: dict, function_selection: str):
                 function_selection=function_selection,
                 units=units,
             )
+    elif chart_selected == Charts.psychrometric.value.name:
+        if (
+            selected_model == Models.PMV_ashrae.name
+            and function_selection == Functionalities.Default.value
+        ):
+            image = psy_ashrae_pmv(inputs=inputs, model="ashrae")
+
+        elif (
+            selected_model == Models.PMV_EN.name
+            and function_selection == Functionalities.Default.value
+        ):
+            image = generate_tdb_hr_chart(inputs=inputs, model="iso", units=units)
+
+    elif chart_selected == Charts.psychrometric_operative.value.name:
+        if (
+            selected_model == Models.PMV_EN.name
+            and function_selection == Functionalities.Default.value
+        ):
+            use_to = chart_selected == Charts.psychrometric_operative.value.name
+            image = generate_operative_chart(inputs=inputs, model="iso")
+        elif (
+            selected_model == Models.PMV_ashrae.name
+            and function_selection == Functionalities.Default.value
+        ):
+            image = psy_ashrae_pmv_operative(inputs=inputs, model="ashrae")
+
+    elif chart_selected == Charts.set_outputs.value.name:
+        if (
+            selected_model == Models.PMV_ashrae.name
+            and function_selection == Functionalities.Default.value
+        ):
+            image = SET_outputs_chart(
+                inputs=inputs, units=units
+            )
+
+    elif chart_selected == Charts.wind_temp_chart.value.name:
+        if (
+            selected_model == Models.PMV_ashrae.name
+            and function_selection == Functionalities.Default.value
+        ):
+            image = speed_temp_pmv(inputs=inputs, model="ashrae", units=units)
+    elif chart_selected == Charts.thl_psychrometric.value.name:
+        if (
+            selected_model == Models.PMV_ashrae.name
+            and function_selection == Functionalities.Default.value
+        ):
+            image = get_heat_losses(inputs=inputs, model="ashrae", units=units)
 
     if (
         selected_model == Models.Adaptive_EN.name
         and function_selection == Functionalities.Default.value
     ):
-        image = adaptive_en_chart(inputs=inputs, units=units)
+        image = adaptive_chart(inputs=inputs, model="iso", units=units)
+    if (
+        selected_model == Models.Adaptive_ASHRAE.name
+        and function_selection == Functionalities.Default.value
+    ):
+        image = adaptive_chart(inputs=inputs, model="ashrae", units=units)
 
     note = ""
     chart: ChartsInfo
